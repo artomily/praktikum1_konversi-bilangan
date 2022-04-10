e1c3ad2e78cac503f60f2a57242f3ce280f049dc
milymax <vararakya@gmail.com>
2022-04-10 22:16:49 +0700

    add bilangan

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Biner from './Biner';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import Oktal from './Oktal';
+import Decimal from './Decimal';
+import Hexadecimal from './Hexadecimal';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Biner />
+    <Oktal />
+    <Decimal />
+    <Hexadecimal />
   </React.StrictMode>,
   document.getElementById('root')
 );
